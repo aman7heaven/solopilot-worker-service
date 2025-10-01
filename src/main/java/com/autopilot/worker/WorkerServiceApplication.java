1e1a21a037825fa407ea6d069ea2d829f8f9c02f
aman7heaven <amansaxena.4654@gmail.com>
2025-10-01 18:33:04 +0530

    structural changes

--- src/main/java/com/autopilot/worker/WorkerServiceApplication.java
+++ src/main/java/com/autopilot/worker/WorkerServiceApplication.java
@@ -3,7 +3,7 @@ package com.autopilot.worker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.autopilot", "com.autopilot.worker"})
 public class WorkerServiceApplication {
 
 	public static void main(String[] args) {
